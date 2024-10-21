035689c33c017c047f8775098d66101a1007e15d
zhiqing <mail@201lab.top>
2024-10-21 11:06:51 +0800

    fix

--- auth.ts
+++ auth.ts
@@ -1,22 +1,23 @@
-import { handlers } from "@/auth";
-import { NextRequest } from "next/server";
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
-const reqWithTrustedOrigin = (req: NextRequest): NextRequest => {
-  const proto = req.headers.get("x-forwarded-proto");
-  const host = req.headers.get("x-forwarded-host");
-  if (!proto || !host) {
-    console.warn("Missing x-forwarded-proto or x-forwarded-host headers.");
-    return req;
-  }
-  const envOrigin = `${proto}://${host}`;
-  const { href, origin } = req.nextUrl;
-  return new NextRequest(href.replace(origin, envOrigin), req);
-};
+import getEnv from "./lib/env-entry";
 
-export const GET = (req: NextRequest) => {
-  return handlers.GET(reqWithTrustedOrigin(req));
-};
-
-export const POST = (req: NextRequest) => {
-  return handlers.POST(reqWithTrustedOrigin(req));
-};
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: "this_is_nezha_dash_web_secret",
+  trustHost: true,
+  pages: {
+    signIn: "/",
+  },
+  providers: [
+    Credentials({
+      credentials: { password: { label: "Password", type: "password" } },
+      authorize: async (credentials) => {
+        if (credentials.password === getEnv("SitePassword")) {
+          return { id: "nezha-dash-auth" };
+        }
+        return null;
+      },
+    }),
+  ],
+});
